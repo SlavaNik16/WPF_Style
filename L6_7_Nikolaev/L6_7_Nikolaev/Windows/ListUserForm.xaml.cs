2acbe6457437bb1b642f5478d7983bbe1cbf8378
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2024-02-22 18:16:16 +0300

    zip -> vs 2022

---
+++ L6_7_Nikolaev/L6_7_Nikolaev/Windows/ListUserForm.xaml.cs
@@ -0,0 +1,122 @@
+﻿using ExcelDataReader;
+using L6_7_Nikolaev.Models;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace L6_7_Nikolaev.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ListUserForm.xaml
+    /// </summary>
+    public partial class ListUserForm : Window
+    {
+        private IExcelDataReader edr;
+        public ListUserForm()
+        {
+            InitializeComponent();
+            list = new ObservableCollection<User>
+            {
+                new User()
+                {
+                    Name = "Вася",
+                    SurName = "Василий",
+                    Role = "Пользователь",
+                    Login = "12345@123",
+                    Password= "Password",
+                },
+                new User()
+                {
+                    Name = "Петя",
+                    SurName = "Петров",
+                    Role = "Пользователь",
+                    Login = "vas@mail.ru",
+                    Password= "12345",
+                },
+                 new User()
+                {
+                    Name = "123",
+                    SurName = "1234567",
+                    Role = "Admin",
+                    Login = "admin",
+                    Password= "admin",
+                },
+            };
+            gridData.ItemsSource = List;
+            
+        }
+        private ObservableCollection<User> list;
+        public ObservableCollection<User> List 
+        { get 
+            { 
+                return list; 
+            } 
+            set
+            {
+                list = value;
+            } 
+        }
+ 
+
+        private void butOpenExel_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "EXCEL Files (*.xlsx)|.xlsx|EXCEL Files 2003 (*.xls)|.xls|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() != true)
+                return;
+            dataGridExel.ItemsSource = readFileExcel(openFileDialog.FileName);
+        }
+
+        private DataView readFileExcel(string fileNames)
+        {
+            var extension = fileNames.Substring(fileNames.LastIndexOf("."));
+            FileStream fs = File.Open(fileNames, FileMode.Open, FileAccess.Read);
+
+            if (extension == ".xlsx")
+                edr = ExcelReaderFactory.CreateOpenXmlReader(fs);
+            if(extension == ".lxs")
+                edr = ExcelReaderFactory.CreateBinaryReader(fs);
+
+            var conf = new ExcelDataSetConfiguration
+            {
+                ConfigureDataTable = _ => new ExcelDataTableConfiguration
+                {
+                    UseHeaderRow = true,
+                }
+            };
+            DataSet dataSet = edr.AsDataSet(conf);
+            DataView dgGrid = dataSet.Tables[0].AsDataView();
+
+            edr.Close();
+            return dgGrid;
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            Export();
+        }
+        void Export()
+        {
+            string path = "export.txt";
+            var sw = new StreamWriter(path);
+            DataTable users = new DataTable("SELECT * FROM [dbo].[user]");
+            for (int i =0; i < users.Rows.Count; i++)
+            {
+                sw.WriteLine($"Name: {users.Rows[i][0]}");
+                sw.WriteLine($"SurName: {users.Rows[i][1]}");
+                sw.WriteLine($"Role: {users.Rows[i][2]}");
+                sw.WriteLine($"Login: {users.Rows[i][3]}");
+                sw.WriteLine($"Password: {users.Rows[i][4]}");
+
+            }
+            sw.Close();
+            Process.Start("notepad.exe",path);
+        }
+    }
+}
---
+++ L6_7_Nikolaev/L6_7_Nikolaev/Windows/TitleForm.xaml.cs
@@ -0,0 +1,78 @@
+﻿using L6_7_Nikolaev.Models;
+using L6_7_Nikolaev.Windows;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L6_7_Nikolaev
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public User user;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            user = new User
+            {
+                Name = "Слава",
+                SurName = "Николаев",
+                Role = "Админ",
+                Login = "SlavaNik",
+                Password = "12345"
+            };
+        }
+
+        private void myWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+       
+
+       
+
+
+        private void butClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void butReg_Click(object sender, RoutedEventArgs e)
+        {
+            RegistForm registForm = new RegistForm();
+            if (registForm.ShowDialog() == true)
+            {
+                if (registForm.passwordBox.Text == user.Password) {
+                    MessageBox.Show("Авторизация пройдена");
+                    ListUserForm listUserForm = new ListUserForm();
+                    this.DataContext = listUserForm;
+                    listUserForm.ShowDialog();
+                }
+                else {
+                    MessageBox.Show("Неверный пароль");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Авторизация не пройдена");
+            }
+        }
+    }
+}
